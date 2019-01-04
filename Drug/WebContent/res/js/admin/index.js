3f72b76a89d61672e97704f47c31ff18a92c9aa4
Administrator <Administrator@WRGHO-20180718H>
2019-01-04 14:27:07 +0800

    index提交

--- Drug/WebContent/res/js/admin/index.js
+++ Drug/WebContent/res/js/admin/index.js
@@ -1,40 +1,56 @@
 $(function(){
-	getSession();
+	showMenu();
 })
+
+function getSession(){
+	var admin = null;
+	$.ajax({ 
+    	url:"./admin/getSession.action",
+        type:'post',
+        async:false,
+        dataType:'json',
+        success:function(mes){
+        	if (mes!=null && mes!="") {
+        		admin = mes;
+			} else {
+				location.href = "./pages/admin/login.html";
+			}
+        },
+        error: function(){
+        	location.href = "./pages/admin/login.html";
+        }
+	});
+	return admin;
+}
 
 /**
  *获取Session
  */
-function getSession(){
-	$.ajaxSetup({
+function showMenu(){
+	/*$.ajaxSetup({
 		async:false,
-	})
-	//得到Session
-	var url = "admin/getSession.action";
-	var data = null;
-	var ids=null;
-	$.post(url,data,function(mes){
-		var urls = "module/findByModule.action";
-		var datas = {"adminId":mes.adminId};                                      
-		$.post(urls,datas,function(mess){
-			$.each(mess,function(index,item){
-				$("#LAY-system-side-menu").append('<li class="layui-nav-item">'
-				+'<a href="javascript:;"><i class="fa fa-plug" aria-hidden="true"></i><span> '+item.modeName+'</span></a>'
-	            +'<dl id="'+item.modeId+'" class="layui-nav-child">'
-	            +'</dl>'
-	            +'</li>');
-				var urlss = "module/findByZiModule.action";
-				var datass = {"modeId":item.modeId,"adminId":mes.adminId};
-				$.post(urlss,datass,function(mes){
-					$.each(mes,function(indexs,items){
-						$("#"+item.modeId).append("<dd><a href='javascript:;' kit-target data-options={url:'./"+items.modeUrl+"',icon:'&#xe658;',title:'"+items.modeName+"',id:'"+items.modeId+"'}><i class='layui-icon'>&#xe614;</i><span> "+items.modeName+"</span></a></dd>")
-					})
-				},"json")
-			});
-			layui.use('element', function() { var element = layui.element; element.init();});
-		},"json")
-	},"json")
-	$.ajaxSetup({
-		async:true,
-	})
+	})*/
+	
+	var admin = getSession();
+	if (admin != null) {
+		$("#sessionShowId>a").html('<img src="./'+admin.adminPictrue+'" class="layui-nav-img"> '+admin.adminName);
+	} else {
+		return false;
+	}
+	$.post("./module/findByModule.action",{"adminId":admin.adminId},function(mes){
+		$.each(mes,function(index,item){
+			$("#LAY-system-side-menu").append('<li class="layui-nav-item">'
+			+'<a href="javascript:;"><i class="fa fa-plug" aria-hidden="true"></i><span> '+item.modeName+'</span></a>'
+            +'<dl id="'+item.modeId+'" class="layui-nav-child">'
+            +'</dl>'
+            +'</li>');
+			$.post("./module/findByZiModule.action",{"modeId":item.modeId,"adminId":admin.adminId},function(mess){
+				$.each(mess,function(indexs,items){
+					$("#"+item.modeId).append("<dd><a href='javascript:;' kit-target data-options={url:'./"+items.modeUrl+"',icon:'&#xe658;',title:'"+items.modeName+"',id:'"+items.modeId+"'}><i class='layui-icon'>&#xe614;</i><span> "+items.modeName+"</span></a></dd>")
+				})
+			},"json")
+		});
+		layui.use('element', function() { var element = layui.element; element.init();});
+	},"json");
+	
 }
